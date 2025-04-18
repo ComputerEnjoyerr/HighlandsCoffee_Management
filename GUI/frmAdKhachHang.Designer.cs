67e3ff82dd3447b23cd8bd8dbf9b23697b9f8848
ThanhPhu <phutram2005@gmail.com>
2025-04-18 08:02:21 +0700

    Hiển thị dữ liệu customer. Thêm dbml

--- GUI/frmAdKhachHang.Designer.cs
+++ GUI/frmAdKhachHang.Designer.cs
@@ -31,7 +31,7 @@
             this.textBox12 = new System.Windows.Forms.TextBox();
             this.lbTim = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
             this.pnlMain = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -49,7 +49,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
             this.pnlMain.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,7 +58,7 @@
             this.textBox12.Location = new System.Drawing.Point(136, 242);
             this.textBox12.Margin = new System.Windows.Forms.Padding(4);
             this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(280, 24);
+            this.textBox12.Size = new System.Drawing.Size(280, 32);
             this.textBox12.TabIndex = 24;
             // 
             // lbTim
@@ -67,7 +67,7 @@
             this.lbTim.Location = new System.Drawing.Point(11, 245);
             this.lbTim.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbTim.Name = "lbTim";
-            this.lbTim.Size = new System.Drawing.Size(117, 18);
+            this.lbTim.Size = new System.Drawing.Size(172, 26);
             this.lbTim.TabIndex = 23;
             this.lbTim.Text = "Tìm khách hàng:";
             // 
@@ -80,16 +80,18 @@
             this.button1.Text = "Tìm";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dgvKhachHang
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvKhachHang.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 277);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(949, 372);
-            this.dataGridView1.TabIndex = 21;
+            this.dgvKhachHang.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Location = new System.Drawing.Point(13, 277);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.RowHeadersWidth = 62;
+            this.dgvKhachHang.Size = new System.Drawing.Size(949, 372);
+            this.dgvKhachHang.TabIndex = 21;
             // 
             // pnlMain
             // 
@@ -158,7 +160,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(523, 63);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(141, 26);
+            this.comboBox1.Size = new System.Drawing.Size(141, 34);
             this.comboBox1.TabIndex = 2;
             // 
             // textBox7
@@ -166,7 +168,7 @@
             this.textBox7.Location = new System.Drawing.Point(523, 32);
             this.textBox7.Margin = new System.Windows.Forms.Padding(4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(141, 24);
+            this.textBox7.Size = new System.Drawing.Size(141, 32);
             this.textBox7.TabIndex = 1;
             // 
             // label7
@@ -175,7 +177,7 @@
             this.label7.Location = new System.Drawing.Point(416, 38);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(47, 18);
+            this.label7.Size = new System.Drawing.Size(70, 26);
             this.label7.TabIndex = 0;
             this.label7.Text = "Điểm:";
             // 
@@ -185,7 +187,7 @@
             this.label5.Location = new System.Drawing.Point(416, 71);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 18);
+            this.label5.Size = new System.Drawing.Size(70, 26);
             this.label5.TabIndex = 0;
             this.label5.Text = "Hạng:";
             // 
@@ -194,7 +196,7 @@
             this.textBox4.Location = new System.Drawing.Point(147, 128);
             this.textBox4.Margin = new System.Windows.Forms.Padding(4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(141, 24);
+            this.textBox4.Size = new System.Drawing.Size(141, 32);
             this.textBox4.TabIndex = 1;
             // 
             // label4
@@ -203,7 +205,7 @@
             this.label4.Location = new System.Drawing.Point(32, 134);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 18);
+            this.label4.Size = new System.Drawing.Size(116, 26);
             this.label4.TabIndex = 0;
             this.label4.Text = "Điện thoại:";
             // 
@@ -212,7 +214,7 @@
             this.textBox3.Location = new System.Drawing.Point(147, 96);
             this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(242, 24);
+            this.textBox3.Size = new System.Drawing.Size(242, 32);
             this.textBox3.TabIndex = 1;
             // 
             // label3
@@ -221,7 +223,7 @@
             this.label3.Location = new System.Drawing.Point(32, 102);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 18);
+            this.label3.Size = new System.Drawing.Size(74, 26);
             this.label3.TabIndex = 0;
             this.label3.Text = "Email:";
             // 
@@ -230,7 +232,7 @@
             this.textBox1.Location = new System.Drawing.Point(147, 64);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(242, 24);
+            this.textBox1.Size = new System.Drawing.Size(242, 32);
             this.textBox1.TabIndex = 1;
             // 
             // label1
@@ -239,7 +241,7 @@
             this.label1.Location = new System.Drawing.Point(32, 70);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 18);
+            this.label1.Size = new System.Drawing.Size(111, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Họ và tên:";
             // 
@@ -248,7 +250,7 @@
             this.textBox2.Location = new System.Drawing.Point(147, 32);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(141, 24);
+            this.textBox2.Size = new System.Drawing.Size(141, 32);
             this.textBox2.TabIndex = 1;
             // 
             // label2
@@ -257,26 +259,26 @@
             this.label2.Location = new System.Drawing.Point(32, 38);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 18);
+            this.label2.Size = new System.Drawing.Size(166, 26);
             this.label2.TabIndex = 0;
             this.label2.Text = "Mã khách hàng:";
             // 
             // frmAdKhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 661);
             this.Controls.Add(this.textBox12);
             this.Controls.Add(this.lbTim);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvKhachHang);
             this.Controls.Add(this.pnlMain);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAdKhachHang";
             this.Text = "frmAdKhachHang";
             this.Load += new System.EventHandler(this.frmAdKhachHang_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
             this.pnlMain.ResumeLayout(false);
             this.pnlMain.PerformLayout();
             this.ResumeLayout(false);
@@ -289,7 +291,7 @@
         private System.Windows.Forms.TextBox textBox12;
         private System.Windows.Forms.Label lbTim;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvKhachHang;
         private System.Windows.Forms.Panel pnlMain;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox7;
